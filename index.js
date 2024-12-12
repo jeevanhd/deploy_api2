d07ee3b41fd1cb033c085bac1d1a74af28b710a5
jeevanhd <jeevan.hd@kalvium.community>
2024-12-12 09:30:03 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 3010;
+const port = 8080;
+const is_admin = process.env.IS_ADMIN;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req, res) => {
+  if (is_admin) {
+    res.send({
+      message: "Welcome Admin",
+      data: ["Admin Data 1", "Admin Data 2"],
+    });
+  } else {
+    res.send({
+      message: "Welcome User",
+      data: ["User Data 1", " User Data 2", " User Data 3", " User Data"],
+    });
+  }
 });
 
 app.listen(port, () => {
